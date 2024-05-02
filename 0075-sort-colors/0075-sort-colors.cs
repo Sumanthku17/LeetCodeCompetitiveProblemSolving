493fd6c4b63591517b6ee42fc040397d0eee3bfe
Sumanth Kumar <57280048+Sumanthku17@users.noreply.github.com>
2024-05-02 22:36:07 +0530

    Time: 102 ms (36.80%), Space: 46.2 MB (7.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cs
+++ 0075-sort-colors/0075-sort-colors.cs
@@ -1,33 +1,7 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        var zeroCount = 0;
-        var oneCount = 0;
-        var twoCount = 0;
-        
-        for (int i=0; i<nums.Length; i++)
-        {
-            if(nums[i] == 0)
-                zeroCount++;
-            else if(nums[i] == 1)
-                oneCount++;
-            else
-                twoCount++;
-        }
-        int j=0;
-        while(zeroCount>0)
-        {
-            nums[j++] = 0;
-            zeroCount--;
-        }
-        while(oneCount>0)
-        {
-            nums[j++] = 1;
-            oneCount--;
-        }
-        while(twoCount>0)
-        {
-            nums[j++] = 2;
-            twoCount--;
-        }
+        List<int> listNums = new List<int>(nums);
+        listNums = listNums.OrderBy(num => num).ToList();
+        listNums.CopyTo(nums);
     }
 }
