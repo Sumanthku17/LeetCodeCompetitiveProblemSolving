66c39517f42012aa0db0a0a117fa571fc52e9af7
Sumanth Kumar <57280048+Sumanthku17@users.noreply.github.com>
2024-09-16 22:48:41 +0530

    Time: 19 ms (84.65%) | Memory: 26.6 MB (91.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cs
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -1,8 +1,24 @@
 public class Solution {
     public int Fib(int n) {
+//         if(n==0 || n==1)
+//             return n;
+        
+//         return Fib(n-1) +Fib(n-2);
+        
+        int[] arr = new int[n+1];
+        return Fibo(n, arr);
+    }
+    
+    public int Fibo(int n, int[] arr)
+    {
         if(n==0 || n==1)
             return n;
         
-        return Fib(n-1) +Fib(n-2);
+        if(arr[n]!=0)
+            return arr[n];
+        
+        arr[n] = Fibo(n-1, arr) + Fibo(n-2, arr);
+        
+        return arr[n];
     }
 }
