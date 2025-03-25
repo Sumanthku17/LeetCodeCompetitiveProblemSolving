05d78f0206596451e0e7a45c77e09ecbe87dab17
Sumanth Kumar <57280048+Sumanthku17@users.noreply.github.com>
2025-03-26 01:21:17 +0530

    Time: 1 ms (32.34%) | Memory: 47.4 MB (10.45%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cs
+++ 88-merge-sorted-array/merge-sorted-array.cs
@@ -1,30 +1,49 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
         
-        int []nums3 = new int[m+n];
+        // // int []nums3 = new int[m+n];
         
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<m && j<n)
+        // // int i=0;
+        // // int j=0;
+        // // int k=0;
+        // // while(i<m && j<n)
+        // // {
+        // //     if(nums1[i]<nums2[j])
+        // //     {
+        // //         nums3[k++] = nums1[i++];
+        // //     }
+        // //     else
+        // //     {
+        // //         nums3[k++] = nums2[j++];
+        // //     }
+        // // }
+        // // while(i<m)
+        // // {
+        // //     nums3[k++] = nums1[i++];
+        // // }
+        // // while(j<n)
+        // // {
+        // //     nums3[k++] = nums2[j++];
+        // // }
+        // // nums3.CopyTo(nums1, 0);
+
+
+        var p1 = m-1;
+        var p2 = n-1;
+        var i = m+n-1;
+
+        while(p2 >= 0)
         {
-            if(nums1[i]<nums2[j])
+            if(p1>=0 && nums1[p1]>nums2[p2])
             {
-                nums3[k++] = nums1[i++];
+                nums1[i--] = nums1[p1--];
             }
             else
             {
-                nums3[k++] = nums2[j++];
+                nums1[i--] = nums2[p2--];
             }
         }
-        while(i<m)
-        {
-            nums3[k++] = nums1[i++];
-        }
-        while(j<n)
-        {
-            nums3[k++] = nums2[j++];
-        }
-        nums3.CopyTo(nums1, 0);
+
+
     }
 }
