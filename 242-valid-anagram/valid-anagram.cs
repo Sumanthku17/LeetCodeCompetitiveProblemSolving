a46929c638f6e59a30b4b0293b6d22379db43475
Sumanth Kumar <57280048+Sumanthku17@users.noreply.github.com>
2025-06-14 18:58:37 +0530

    Time: 4 ms (74.65%) | Memory: 43.9 MB (53.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.cs
+++ 242-valid-anagram/valid-anagram.cs
@@ -1,20 +1,39 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        List<Char> chList1 = s.ToCharArray().ToList();
-        List<Char> chList2 = t.ToCharArray().ToList();
+        // List<Char> chList1 = s.ToCharArray().ToList();
+        // List<Char> chList2 = t.ToCharArray().ToList();
 
-        foreach(Char ch in chList1)
+        // foreach(Char ch in chList1)
+        // {
+        //     if(chList2.Contains(ch))
+        //     {
+        //         chList2.Remove(ch);
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // }
+
+        // return (chList2.Count() == 0);
+
+        if(s.Length != t.Length)
+            return false;
+
+        Dictionary<char, int> charFreq = new Dictionary<char, int>();
+        foreach(char ch in s)
         {
-            if(chList2.Contains(ch))
-            {
-                chList2.Remove(ch);
-            }
-            else
+            charFreq[ch] = charFreq.GetValueOrDefault(ch, 0) + 1;
+        }
+
+        foreach(char ch in t)
+        {
+            if(!charFreq.ContainsKey(ch) || charFreq[ch] == 0)
             {
                 return false;
             }
+            charFreq[ch]--;
         }
-
-        return (chList2.Count() == 0);
+        return true;
     }
 }
