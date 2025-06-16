189d4dcdfb5b1959801eb08a3bb6d7217809b9f2
Sumanth Kumar <57280048+Sumanthku17@users.noreply.github.com>
2025-06-16 17:58:51 +0530

    Time: 9 ms (52.15%) | Memory: 51.6 MB (54.97%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cs
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cs
@@ -13,9 +13,25 @@ public class Solution {
             numFreq[num] = numFreq.GetValueOrDefault(num, 0)+1;
         }
 
-        return numFreq.OrderByDescending(pair => pair.Value)
-                .Take(k)
-                .Select(pair => pair.Key)
-                .ToArray();
+        PriorityQueue<int, int> minHeap = new PriorityQueue<int, int>();
+
+        foreach(var pair in numFreq)
+        {
+            minHeap.Enqueue(pair.Key, pair.Value);
+            if (minHeap.Count > k) {
+                minHeap.Dequeue();
+            }
+        }
+
+        int[] result = new int[k];
+        for(int i=0; i<k; i++)
+        {
+            result[i] = minHeap.Dequeue();
+        }
+        return result;
+        // return numFreq.OrderByDescending(pair => pair.Value)
+        //         .Take(k)
+        //         .Select(pair => pair.Key)
+        //         .ToArray();
     }
 }
