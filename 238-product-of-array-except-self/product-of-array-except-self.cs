051c991852fa519c2b434f211621dcf0caea063e
Sumanth Kumar <57280048+Sumanthku17@users.noreply.github.com>
2025-06-17 03:13:06 +0530

    Time: 2 ms (73.52%) | Memory: 68.7 MB (14.62%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cs
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -1,38 +1,64 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
         
-        int[] LeftSideProduct = new int[nums.Length];
-        int[] RightSideProduct = new int[nums.Length];
+        // int[] LeftSideProduct = new int[nums.Length];
+        // int[] RightSideProduct = new int[nums.Length];
         
-        for(int i=0;i<nums.Length;i++)
+        // for(int i=0;i<nums.Length;i++)
+        // {
+        //     if(i==0)
+        //     {
+        //         LeftSideProduct[i] = 1;
+        //     }
+        //     else
+        //     {
+        //         LeftSideProduct[i] = LeftSideProduct[i-1] * nums[i-1];
+        //     }
+        // }
+        
+        // for(int i=nums.Length-1;i>=0;i--)
+        // {
+        //     if(i==nums.Length-1)
+        //     {
+        //         RightSideProduct[i] = 1;
+        //     }
+        //     else
+        //     {
+        //         RightSideProduct[i] = RightSideProduct[i+1] * nums[i+1];
+        //     }
+        // }
+        
+        // for(int i=0;i<nums.Length;i++)
+        // {
+        //     nums[i] = LeftSideProduct[i]*RightSideProduct[i];
+        // }
+        
+        // return nums;
+
+        var prefixProduct = new int[nums.Length];
+        var suffixProduct = new int[nums.Length];
+        var result = new int[nums.Length];
+        int product = 1;
+        for(int i=0; i<nums.Length; i++)
         {
-            if(i==0)
-            {
-                LeftSideProduct[i] = 1;
-            }
-            else
-            {
-                LeftSideProduct[i] = LeftSideProduct[i-1] * nums[i-1];
-            }
+            
+            prefixProduct[i] = product;
+            product *= nums[i];
         }
-        
-        for(int i=nums.Length-1;i>=0;i--)
+
+        product = 1;
+        for(int i=nums.Length-1; i>=0; i--)
         {
-            if(i==nums.Length-1)
-            {
-                RightSideProduct[i] = 1;
-            }
-            else
-            {
-                RightSideProduct[i] = RightSideProduct[i+1] * nums[i+1];
-            }
+            
+            suffixProduct[i] = product;
+            product *= nums[i];
         }
-        
-        for(int i=0;i<nums.Length;i++)
+
+        for(int i=0; i<nums.Length; i++)
         {
-            nums[i] = LeftSideProduct[i]*RightSideProduct[i];
+            result[i] = prefixProduct[i] * suffixProduct[i];
         }
-        
-        return nums;
+
+        return result;
     }
 }
