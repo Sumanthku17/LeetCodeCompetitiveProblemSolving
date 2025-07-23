2668239f97bdbe9eea66a6fe69b84d9ef0a36e3c
Sumanth Kumar <57280048+Sumanthku17@users.noreply.github.com>
2025-07-23 17:24:27 +0530

    Time: 19 ms (23.30%) | Memory: 49.9 MB (7.92%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        if(piles ==null || piles.Length ==0)
+        {
+            return -1;
+        }
+        
+        int low =1;
+        int high =piles.Max();
+        int mink =high;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(canComplete(piles, h, mid))
+            {
+                mink=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return mink;
+    }
+
+    public bool canComplete(int[] piles, int h, int mid)
+    {
+        long completedHours = 0;
+        for(int i=0; i<piles.Length; i++)
+        {
+            completedHours += (long)Math.Ceiling((double)piles[i]/mid);
+            if(completedHours > h)
+            {
+                return false;
+            }
+        }
+        return completedHours <= h;
+    }
+}
